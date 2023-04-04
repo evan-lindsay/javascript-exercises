28c3e134b8379a22fe77c53e7ff544ef325a6919
Evan <96845039+evan-lindsay@users.noreply.github.com>
2023-04-04 15:40:57 -0500

    update

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,33 @@
 
 
 
-const sumAll = function() {
+const sumAll = function(...x) {
+    let xArray = Array.from(x)
 
+    if (xArray.contains(String)) {
+        return "ERROR"
+    }
+    
+    
+    else if (xArray[0] > xArray[1])
+    { xArray.sort((a, b) => b - a) 
+
+    }
+
+    if (xArray[0] < 0 || xArray[1] < 0){
+        return "ERROR"
+    }
+
+
+
+    let sumArray = 0
+
+    for (let i = xArray[0]; i <= xArray[1]; i++)
+    {
+        sumArray += i
+    }
+    console.log(xArray)
+    return sumArray
 };
 
 // Do not edit below this line
